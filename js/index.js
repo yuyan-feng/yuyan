94ea7a47637e50792063b3cde48a4130fb204228
bintianf <bintianf@gmail.com>
2019-06-11 19:38:27 -0400

    6.11

--- js/index.js
+++ js/index.js
@@ -22,32 +22,4 @@ $(function() {
     });
   });
 
-  $container.infinitescroll({
-      navSelector: '#page-nav', // selector for the paged navigation
-      nextSelector: '#page-nav a', // selector for the NEXT link (to page 2)
-      itemSelector: '.grid-item', // selector for all items you'll retrieve
-      loading: {
-        finishedMsg: 'No more pages to load.',
-        img: 'http://i.imgur.com/6RMhx.gif'
-      },
-      bufferPx: 160,
-    },
-    // trigger Masonry as a callback
-    function(newElements) {
-      // hide new items while they are loading
-      var $newElems = $(newElements).css({
-        opacity: 0
-      });
-      // ensure that images load before adding to masonry layout
-      waitForvidLoad($newElems.find('video'), function() {
-        $newElems.imagesLoaded(function() {
-          // show elems now they're ready
-          $newElems.animate({
-            opacity: 1
-          });
-          $container.masonry('appended', $newElems, true);
-        });
-      });
-    }
-  );
 });
